652d29240bb29ea33d2aaad63ba150c4521633f0
Abhishek Bhadoriya <127028405+abhishekbhadoriya421@users.noreply.github.com>
2024-01-25 17:19:47 +0530

    Update constants.js
    Update the link of resume

--- src/data/constants.js
+++ src/data/constants.js
@@ -14,7 +14,7 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/abhishekbhadoriya421",
   resume:
-    "https://drive.google.com/file/d/19QsUfKT9rBKonaASg2bUc1dpUoSBcI0_/view",
+    "https://drive.google.com/file/d/1u8tJhEKTq_SlDUs5MwonYWExNA_Y_yvO/view?usp=sharing",
   linkedin: "https://www.linkedin.com/in/abhishek-bhadoriya-5445071a0/",
   twitter: "#",
   insta: "https://www.instagram.com/_____ab_hi_sh_ek_____/",
